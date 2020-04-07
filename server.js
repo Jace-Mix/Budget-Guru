39fd8d3493ac98c315f53b0d56dcdc9a9b0a7de0
Jace-Mix <jace.mixon@knights.ucf.edu>
2020-04-06 21:43:42 -0400

    moved dotenv

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-require('dotenv').config();
 
 // Express setup
 const app = express();
